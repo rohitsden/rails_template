a71b1a8e5b602a6a056cc59cd1166ea41523418d
Rohit Menon <rohitsden@gmail.com>
2020-06-21 20:42:54 -0400

    add logout link for home view

--- commands.sh
+++ commands.sh
@@ -5,6 +5,7 @@ rails generate devise User
 rails g devise:views
 yarn add bootstrap jquery popper.js
 rails g controller Home index
+echo "<% if user_signed_in? %><%= link_to('Logout', destroy_user_session_path, method: :delete) %><% end %>" >> ./app/views/home/index.html.erb
 mv ./templates/js/environment.js ./config/webpack/environment.js
 mv ./templates/js/application.js ./app/javascript/packs/application.js
 mv ./templates/css/application.css ./app/assets/stylesheets/application.css
